7c6e22802a9da75e5c6fec2cc0d271f22499b350
everluma <farjanaaktarmonisha@gmail.com>
2026-02-23 13:15:42 +0600

    Interview count added

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-console.log('It is Connected');
+// console.log('It is Connected');
+const interviewDisplay = document.getElementById('interview-count');
+
+const rejectedDisplay = document.getElementById('rejected-count');
+
+const jobCards = document.querySelectorAll('.job-card');
+
+
+let currentInterviewCount = 0;
+let currentRejectedCount = 0;
+
+
+jobCards.forEach((card) => {
+    const interviewBtn = card.querySelector('.job-button button:nth-child(1)');
+
+    const statusBtn = card.querySelector('button:not(.job-button button)');
+
+
+    interviewBtn.addEventListener('click', function() {
+        if (statusBtn.innerText !== "Interview") {
+            currentInterviewCount++;
+            interviewDisplay.innerText = currentInterviewCount;
+
+
+            statusBtn.innerText = 'Interview';
+            statusBtn.style.backgroundColor = "#28a745";
+            statusBtn.style.color = "white";
+
+
+            interviewBtn.disabled = true;
+            interviewBtn.style.opacity = "0.5";
+            interviewBtn.style.cursor = "not-allowed";
+        }
+    })
+})
