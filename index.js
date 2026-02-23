f026f97fbe3f521d72f8f983be5bcd19b178e3d2
everluma <farjanaaktarmonisha@gmail.com>
2026-02-23 21:01:29 +0600

    Dashboard Update Function Added in Js file

--- index.js
+++ index.js
@@ -1,70 +1,63 @@
 // console.log('It is Connected');
+
 const interviewDisplay = document.getElementById('interview-count');
 
 const rejectedDisplay = document.getElementById('rejected-count');
 
 const jobCards = document.querySelectorAll('.job-card');
 
+const filterButton = document.querySelectorAll('.btn');
 
-let currentInterviewCount = 0;
-let currentRejectedCount = 0;
+const jobCountText = document.querySelector('.count p');
 
-
-jobCards.forEach((card) => {
-
-    // Select the Button 
-
-    // interview button 
-    const interviewBtn = card.querySelector('.job-button button:nth-child(1)');
-
-    // Rejected button 
-    const rejectedBtn = card.querySelector('.job-button button:nth-child(2)');
-
-    const statusBtn = card.querySelector('button:not(.job-button button)');
+const emptyState = document.querySelector('.empty-state');
 
 
-    interviewBtn.addEventListener('click', function() {
-        if (statusBtn.innerText !== "Interview") {
-
-            // age theke rejected button a click kora thakle eta hobe 
-
-            if (statusBtn.innerText === "Rejected") {
-                currentRejectedCount--;
-                rejectedDisplay.innerText = currentRejectedCount;
-            }
+// let currentInterviewCount = 0;
+// let currentRejectedCount = 0;
 
 
-            currentInterviewCount++;
-            interviewDisplay.innerText = currentInterviewCount;
+const jobs = [];
 
-
-            statusBtn.innerText = 'Interview';
-            statusBtn.style.backgroundColor = "#28a745";
-            statusBtn.style.color = "white";
-
-
-            interviewBtn.disabled = true;
-            interviewBtn.style.opacity = "0.5";
-            interviewBtn.style.cursor = "not-allowed";
-        }
-    });
-
-
-    // For Rejected Button 
-    rejectedBtn.addEventListener('click', function(){
-        if (statusBtn.innerText !== "Rejected") {
-            if (statusBtn.innerText === "Interview") {
-                currentInterviewCount--;
-                interviewDisplay.innerText = currentInterviewCount;
-            }
-
-            currentRejectedCount++;
-            rejectedDisplay.innerText = currentRejectedCount;
-
-
-            statusBtn.innerText = "Rejected";
-            statusBtn.style.backgroundColor = "#dc3545";
-            statusBtn.style.color = "white";
-        }
-    });
+jobCards.forEach(card => {
+    jobs.push({
+    id:card.dataset.id,
+    element:card,
+    status:"All"
 });
+
+});
+
+
+
+// DashBoard update Function 
+
+function updateDashboard() {
+
+    const interviewCount =jobs.filter(job => job.status === "Interview").length;
+    const rejecteCount =jobs.filter(job => job.status === "Rejected").length;
+
+}
+
+
+
+// Render function
+
+function renderJobs(filterType = "All") {
+let visibleCount = 0;
+
+jobs.forEach(job => {
+    if (filterType === "All") {
+        job.element.style.display = "block";
+        visibleCount++;
+    } else if (job.status === filterType) {
+        job.element.style.display = "block";
+    }
+})
+}
+
+
+
+
+
+           
